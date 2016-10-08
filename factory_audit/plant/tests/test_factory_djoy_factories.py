8c17a9158e7057d87f56a2645ed0d9f8112ac6c0
James Cooke <jamescooke@users.noreply.github.com>
2016-10-08 13:41:24 +0100

    Add factory djoy factories

---
+++ factory_audit/plant/tests/test_factory_djoy_factories.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from factory_djoy import UserFactory
+
+from ..factory_djoy_factories import ItemFactory  #, UserFactory
+from ..models import Item
+
+
+class TestItemFactory(TestCase):
+
+    def test_default(self):
+        """
+        Factory Djoy: Plant.Item: YELLOW (raises ValidationError)
+        """
+        with self.assertRaises(ValidationError) as cm:
+            ItemFactory()
+
+        self.assertIn('cannot be blank', str(cm.exception))
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class TestUserFactory(TestCase):
+
+    user_model = get_user_model()
+
+    def test_default(self):
+        """
+        Factory Djoy; User Model: GREEN (makes a valid instance)
+        """
+        result = UserFactory.create_batch(10)
+
+        self.assertEqual(self.user_model.objects.count(), 10)
+        for user in result:
+            self.assertIsNone(user.full_clean())
